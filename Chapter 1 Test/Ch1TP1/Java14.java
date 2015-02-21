b6049ad77efcb105df17980650229eb7b0f44ddb
zachpanz88 <zachary@panzarino.com>
2015-02-21 11:58:30 -0500

    Chapter 1 Test

---
+++ Chapter 1 Test/Ch1TP1/Java14.java
@@ -0,0 +1,16 @@
+//Zach Panzarino
+//2-16-15
+//Chapter 1 Test Problem 1
+
+public class Java14{
+	public static void main (String[] args){
+		System.out.println("Java!");
+	}
+}
+
+/* Program Output
+ --------------------Configuration: Ch1TP1 - JDK version 1.7.0_04 <Default> - <Default>--------------------
+Java!
+
+Process completed.
+*/
---
+++ Chapter 1 Test/Ch1TP2/Authors14.java
@@ -0,0 +1,20 @@
+//Zach Panzarino
+//2-16-15
+//Chapter 1 Test Problem 2
+
+public class Authors14{
+	public static void main (String[] args){
+		System.out.println("The first author is John Lewis.");
+		System.out.println("The second author is William Loftus.");
+		System.out.println("The third and final author is Cara Cocking.");
+	}
+}
+
+/* Program Output
+--------------------Configuration: Ch1TP2 - JDK version 1.7.0_04 <Default> - <Default>--------------------
+The first author is John Lewis.
+The second author is William Loftus.
+The third and final author is Cara Cocking.
+
+Process completed.
+*/
